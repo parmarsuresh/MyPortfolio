f5246511d3a6ab5a5897d9af67ce0d59c2acc0d6
parmarsuresh <parmarsuresh542001@gmail.com>
2025-12-09 21:43:37 +0530

    update main.jsx file for refesh any page not show 404 error page on gitub page site

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/MyPortfolio/">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
